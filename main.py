9a217b6af1a3010c70d3a6de636394a1ab2f2401
damiura <10miudai29@gmail.com>
2023-03-09 14:41:08 +0900

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.title("hello")
+def hello():
+    page_id == "hello"
+    st.title("ログインに成功したということにしよう")
+    st.date_input('日付インプット')
+
+def LoginPage():
+    st.title("パスワードベースの SSO で利用くだせえ")
+    username = st.text_input("ユーザー名を入力してください")
+    password = st.text_input("パスワードを入力してください", type='password')
+    st.selectbox('コンボボックス', ('KARAAGE', 'GYOUZA'))
+    st.checkbox('チェックボックス')
+    st.radio('ラジオボタン', ('A', 'B'))
+    
+    if st.button(label='ログイン'):
+        st.write(username, "さんログインに成功ということで",unsafe_allow_html=True)
+        
+LoginPage()
